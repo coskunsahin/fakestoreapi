cd1d9c7e799b41c7ae461da5c5c0811abb7e9b2f
COSKUN <coskunsahin1981@yahoo.com>
2023-01-01 04:52:45 +0300

    ee

---
+++ fakestoreapi.api/Startup.cs
@@ -0,0 +1,78 @@
+using fakestoreapi.api.Services;
+using fakestoreapi.application;
+using fakestoreapi.application.Common.Interfaces;
+using fakestoreapi.infrastructure;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.AzureAD.UI;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NSwag.Generation.Processors.Security;
+using NSwag;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fakestoreapi.api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<ICurrentUserService, CurrentUserService>();
+
+            services.AddInfrastructure(Configuration);
+            services.AddApplication();
+            services.AddCors();
+
+            //services.AddAuthentication()
+            //  .AddIdentityServerJwt();
+            //services.AddAuthentication()
+
+            services.AddOpenApiDocument(configure =>
+            {
+                configure.Title = "= API";
+
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseCors(x => x
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .SetIsOriginAllowed(origin => true) 
+                        .AllowCredentials()); 
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
